d39a201eede03b8b8f17f3d949870629fe013d91
jonad <jonadjons@gmail.com>
2017-11-20 07:56:56 -0800

    Update: add additional documentation to utils.py file

--- utils/utils.py
+++ utils/utils.py
@@ -1,30 +1,17 @@
 import numpy as np
 import keras.backend as K
 import string
-#from nltk.corpus import stopwords
+from nltk.corpus import stopwords
 import re
 from numpy.linalg import norm
-from sklearn.model_selection import train_test_split
 import csv
 
-#stop_words = stopwords.words('english')
+stop_words = stopwords.words('english')
 
-def split_dataset(dataset, seed, indices):
-    '''
-    Split the dataset into train, test, and validation
-    :param dataset: the dataset
-    :param seed: seed
-    :param indices: indices array
-    :return: return tuple of trai, test and validation data.
-    '''
-    train, validation, test = np.split(dataset.sample(frac=1, random_state=seed),
-                                     [int(indices[0]* len(dataset)), int(indices[1] * len(dataset))])
-
-    return train, validation, test
 
 def reshape_data(sentence_1_data, sentence_2_data, y_data):
     '''
-    Reshape the input data
+    Reshapes the input data.
     :param sentence_1_data: sentence 1
     :param sentence_2_data: sentence 2
     :param y_data: output
@@ -44,21 +31,13 @@ def reshape_data(sentence_1_data, sentence_2_data, y_data):
     
     return sentence_1, sentence_2, y
 
-def accuracy(y_true, y_pred, threshold_shift=0):
-    '''
-    Compute accuracy score
-    :param y_true: actual output
-    :param y_pred: predicted output
-    :param threshold_shift: threshold
-    :return: accuracy score
-    '''
-    y_pred = K.clip(y_pred, 0, 1)
-    y_pred = K.round(y_pred + threshold_shift)
-    return K.mean(K.equal(y_true, y_pred))
+
 
 def clean_str(txt):
     '''
-    clean a sentence by removing all non alphanumeric token.
+    Cleans a text by removing all non alphanumeric characters.
+    :param txt: String, the input sentence.
+    :return: A cleaned sentence.
     '''
     txt = str(txt)
     txt = re.sub(r"[^A-Za-z0-9(),!?\'\`&%]", " ", txt)
@@ -80,20 +59,22 @@ def clean_str(txt):
 
 def remove_punctuation(txt):
     '''
-    Remove punctuation in a given text.
-    :param txt: a text string
-    :return: an array of word which is not a punctuation.
+    Removes punctuations in a given sentence.
+    :param txt: String, input sentence.
+    :return: a list of words from the input sentence without punctuation.
     '''
     return [w for w in txt if w not in string.punctuation]
 
 
 def remove_stopwords(txt):
     '''
-    Remove english stopword in a given text.
-    :param txt: a string of text.
-    :return: an array of word which is not english stopwords.
+    Removes english stopword in a given sentence.
+    :param txt: String, the input sentence.
+    :return: A list of words from the input sentence without any stopwords.
     '''
     return [w for w in txt if w not in stop_words]
+
+
 
 def fbeta(y_true, y_pred, threshold_shift=0, beta=1):
     '''
@@ -123,41 +104,24 @@ def fbeta(y_true, y_pred, threshold_shift=0, beta=1):
     return (beta_squared + 1) * (precision * recall) / (beta_squared * precision + recall + K.epsilon())
 
 
-def get_w2v_vector(model, sentence, vector_size):
-    '''
-    Get the word2vec vector of a given sentence.
-    :param model: word2vec model
-    :param sentence: sentence
-    :param vector_size: vector size
-    :return: a word2vec vector of a given sentence
-    '''
-    
-    sentence_len = len(sentence)
-    sentence2vec = np.zeros(shape=(sentence_len, vector_size), dtype='float32')
-    for i in range(sentence_len):
-        word = sentence[i]
-        word_vector = model[word]
-        sentence2vec[i] = word_vector
-    
-    return sentence2vec
 
 
 def cosine_sim(u, v):
     '''
-    Compute the cosine similarity between two vectors
-    :param u: a vector
-    :param v: a vector
-    :return: the cosine similarity score.
+    Computes the cosine similarity between two vectors u and v.
+    :param u: Numpy ndarray, the vector u.
+    :param v: Numpy ndarray, the vector v.
+    :return: Float between 0 and 1, the cosine similarity score between the vector u and v.
     '''
     return np.dot(u,v) / (norm(u)*norm(v))
 
 
 def similarity(q1, q2):
     '''
-    Compute the similarity score between arrays of vectors.
-    :param q1: array of vectors
-    :param q2: array of vectors
-    :return: vector of similarity score
+    Computes the similarity score between lists of vectors.
+    :param q1: List of the first vector.
+    :param q2: List of the second vector.
+    :return: A list of similarity score between vectors in q1 and q2.
     '''
     sim = []
     for el1, el2 in zip(q1, q2):
@@ -167,10 +131,9 @@ def similarity(q1, q2):
 
 def save_result(filename, results):
     '''
-    Save a dictionary of data into a file.
-    :param filename: the file name
+    Saves a dictionary of data into a file.
+    :param filename: String, the filename
     :param results: The dictionary to save into the file
-    :return: None
     '''
     keys = results[0].keys()
     with open(filename, 'w') as f:
@@ -178,14 +141,54 @@ def save_result(filename, results):
         dict_writer.writeheader()
         dict_writer.writerows(results)
 
-def split_dataset_xgb(x, y, test_size, seed):
-  '''
-    Split the data into train and test data.
-    :param x: input data
-    :param y: target data
-    :param test_size: proportion of the test size
-    :param seed: seed
-    :return: train and test dataset
-  '''
-  X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=seed)
-  return X_train, X_test, y_train, y_test
+def make_accuracy(threshold_shift):
+    '''
+    Creates an accuracy function with a given threshold.
+    :param threshold_shift: Float, threshold
+    :return: An accuracy function
+    '''
+    def accuracy(y_true, y_pred):
+        '''
+        Compute accuracy score
+        :param y_true: actual output
+        :param y_pred: predicted output
+        :return: accuracy score
+        '''
+        y_pred = K.clip(y_pred, 0, 1)
+        y_pred = K.round(y_pred + threshold_shift)
+        return K.mean(K.equal(y_true, y_pred))
+    return accuracy
+    
+
+def make_fbeta(threshold_shift):
+    '''
+    Creates fbeta function with a given threshold.
+    :param threshold_shift: Float, threshold shift
+    :return: An fbeta function
+    '''
+    def fbeta(y_true, y_pred, beta=1):
+        '''
+        Computes the fbeta score.
+        :param y_true: y_true
+        :param y_pred: predicted value
+        :param beta: beta value.
+        :return: fbeta score.
+        '''
+        # just in case of hipster activation at the final layer
+        y_pred = K.clip(y_pred, 0, 1)
+        y_true = K.clip(y_true, 0, 1)
+        
+        # shifting the prediction threshold from .5 if needed
+        y_pred_bin = K.round(y_pred + threshold_shift)
+        
+        tp = K.sum(K.round(y_true * y_pred_bin)) + K.epsilon()
+        fp = K.sum(K.round(K.clip(y_pred_bin - y_true, 0, 1)))
+        fn = K.sum(K.round(K.clip(y_true - y_pred, 0, 1)))
+        
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        
+        beta_squared = beta ** 2
+        return (beta_squared + 1) * (precision * recall) / (beta_squared * precision + recall + K.epsilon())
+    
+    return fbeta
